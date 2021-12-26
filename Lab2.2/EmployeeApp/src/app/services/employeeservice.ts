c3eb1997e929b0ad8eb535ebcd9aa84c5a986032
vaishnavirgh02 <52289582+vaishnavirgh02@users.noreply.github.com>
2021-12-26 11:35:55 +0530

    made changes

--- Lab2.2/EmployeeApp/src/app/services/employeeservice.ts
+++ Lab2.2/EmployeeApp/src/app/services/employeeservice.ts
@@ -6,10 +6,7 @@ export class EmployeeService{
 
  store=[
    new Employee(2,'abc',10000,"JAVA"),
-   new Employee(1,'vibhor',20000,"Angular"),
-   new Employee(5,'shubham',20000,"Js"),
-   new Employee(4,'ritu',20000,"Java"),
-   new Employee(3,'shreya',20000,"Angular")
+  
 ];   
  
  
